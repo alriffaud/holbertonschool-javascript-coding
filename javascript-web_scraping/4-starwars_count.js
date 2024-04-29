5124a76e66326d8fd9132274fc8f507882950633
Alberto Riffaud <alriffaud@gmail.com>
2024-04-29 18:14:20 -0300

    Modification of 4-starwars_count.js file.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 // This script prints the number of movies where the character “Wedge Antilles” is present.
 const request = require('request');
 const apiUrl = process.argv[2];
-request(apiUrl, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error.message);
   } else {
