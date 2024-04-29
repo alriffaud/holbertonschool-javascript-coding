001f255bce29a745001bd9e1b6a9030c5f9880fa
Alberto Riffaud <alriffaud@gmail.com>
2024-04-29 08:56:22 -0300

    Creation of 1-writeme.js file.

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// This script writes a string to a file.
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+const filePath = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(filePath, str, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(str);
+});
