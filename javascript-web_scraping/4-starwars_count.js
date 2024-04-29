7a066a3ddfd2e3c46373b03b495fe376abb47f2e
Alberto Riffaud <alriffaud@gmail.com>
2024-04-29 11:51:18 -0300

    Modification of 4-starwars_count.js file.

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,36 +1,24 @@
 #!/usr/bin/node
 // This script prints the number of movies where the character “Wedge Antilles” is present.
-const request = require('request');
+const request = require("request");
 
-if (process.argv.length !== 2) {
-  console.error('Usage: node 4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
 const apiUrl = process.argv[2];
 
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    return;
-  }
-  try {
-    const films = JSON.parse(body).results;
+    console.error(error.message);
+  } else {
+    const data = JSON.parse(body);
     let count = 0;
-    if (films) {
-      for (const film of films) {
-        if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-          count++;
+    for (const result of results) {
+      if (result.characters) {
+        for (const character of result.characters) {
+          if (character.includes("18")) {
+            count++;
+          }
         }
       }
-      console.log(count);
-    } else {
-      console.error('films not found');
     }
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
+    console.log(count);
   }
 });
