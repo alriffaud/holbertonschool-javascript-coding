eeb27d5272ca64d42b1eee90e71f002de39a915e
Alberto Riffaud <alriffaud@gmail.com>
2024-05-07 09:19:37 -0300

    Creation of 0-console.js file.

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// This script defines a function named displayMessage that prints in STDOUT the string argument.
+
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
