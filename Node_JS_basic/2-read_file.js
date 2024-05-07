b3015f584174896686f057d1e093c2f692a33faf
Alberto Riffaud <alriffaud@gmail.com>
2024-05-07 11:33:45 -0300

    Creation of 2-read_file.js file.

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+// This script using the database database.csv, creates a function countStudents
+// in the file 2-read_file.js.
+const path = require('path');
+const fs = require('fs');
+
+const countStudents = (file) => {
+  let data;
+  try {
+    data = fs.readFileSync(path.normalize(file), 'utf-8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+  const dataArray = data
+    .trim()
+    .split('\n')
+    .map((line) => line.split(','));
+  console.log(
+    `Number of students: ${dataArray.length ? dataArray.length - 1 : 0}`,
+  );
+  const fields = {};
+  dataArray.forEach((line) => {
+    const fieldName = line[3];
+    if (!fields[fieldName]) fields[fieldName] = [];
+    fields[fieldName].push(line[0]);
+  });
+  delete fields.field;
+  for (const fieldName in fields) {
+    if (fieldName) {
+      console.log(
+        `Number of students in ${fieldName}: ${
+          fields[fieldName].length
+        }. List: ${fields[fieldName].join(', ')}`,
+      );
+    }
+  }
+};
+
+module.exports = countStudents;
