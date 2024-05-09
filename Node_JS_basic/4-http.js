699fca1211168278af0e79d8feab55450dfe96b7
Alberto Riffaud <alriffaud@gmail.com>
2024-05-09 10:00:24 -0300

    Creation of4-http.js  file.

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+// This script creates a small HTTP server using the http module.
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!\n');
+});
+
+// eslint-disable-next-line jest/require-hook
+app.listen(1245);
+
+module.exports = app;
