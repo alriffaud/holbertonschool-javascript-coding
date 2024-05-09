f67290d498e70b348739fb6d7611aa7c9536e386
Alberto Riffaud <alriffaud@gmail.com>
2024-05-09 10:29:30 -0300

    Creation of5-http.js file.

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+// This script creates a small HTTP server using the http module.
+const http = require('http');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+}).listen(1245);
+
+module.exports = app;
